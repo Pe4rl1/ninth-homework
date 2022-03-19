ae7257958159a870dd235772d1fa28b530c4f78b
Pearl <alexander.multan>
2022-03-19 15:42:14 +0300

    .

--- db.py
+++ db.py
@@ -1,11 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 
+user = 'postgres'
+passwd = '21012006'
+host = 'localhost'
+port = '5432'
+
 # Postgres
-# url = f'postgresql://{user}:{passwd}@{host}:{port}/{db}'
+ url = f'postgresql://{user}:{passwd}@{host}:{port}/{db}'
 
 # SQLite
-url = 'sqlite:///local.db'
+ #url = 'sqlite:///local.db'
 
 engine = create_engine(url, echo=False)
 Base = declarative_base()
